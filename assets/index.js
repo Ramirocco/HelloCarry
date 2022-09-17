b06ce4469f7522a5b4dc07bcc51a7122eecf8f33
Ramirocco <105560507+Ramirocco@users.noreply.github.com>
2022-09-16 22:51:45 -0300

    falta poder enlazar los checkbox

--- assets/index.js
+++ assets/index.js
@@ -1,45 +1,66 @@
+//Variables y constantes
+const comision = 0.15;
+//form
+let formularioCarry = document.getElementById("formularioCarry"); 
 //variables de partes de tarjeta
-let provinciaDestinoSelec;
-let provinciaOrigenSelec;
-const comision = 0.15
+let tituloTarjetaCarry = document.getElementById("tituloTarjetaCarry");
+let mensajeCarry = document.getElementById("mensajeCarry");
+let mascotaCarry = document.getElementById("mascotasCarry");
+let fumarCarry = document.getElementById("fumarCarry");
+let maletaCarry = document.getElementById("maletaCarry");
+let peajesCarry = document.getElementById("peajesCarry");
+let precioCarry = document.getElementById("precioCarry");
+//mensajes para mostrar
+let mensajeFumador= "usted podria fumar en el vehiculo";
+let mensajeNoFumador = "usted NO podria fumar en el vehiculo";
+let mensajeAnimales= "usted podria llevar mascotas";
+let mensajeNoAnimales = "usted NO podria llevar mascotas";
+let mensajeMaleta= "usted podria llevar maleta";
+let mensajeNoMaleta = "usted NO podria llevar maletas";
+let mensajePeaje= "Los precios incluyen los peajes y gastos";
+let mensajeNoPeaje = "Los precios NO incluyen los peajes y gastos";
+
 //HTMLColection
 let infoFormulario;
-let preciototal
 
+let precioTotal;
+let incluyePeajes;
 
-let formularioCarry = document.getElementById("formularioCarry");
+function precioTotalCarry() {
+    precioTotal = Number(infoFormulario[10].lastElementChild.value) * comision
+};
+
+//codigo ejecutable
 formularioCarry.addEventListener("submit", (e) => {
     e.preventDefault();
     infoFormulario = e.target.children;
     //ver info//
     console.log(infoFormulario);
     //trayecto en tarjeta
-    provinciaOrigenSelec = infoFormulario[1].lastElementChild.value;
-    provinciaDestinoSelec = infoFormulario[2].lastElementChild.value;
+    let provinciaOrigenSelec = infoFormulario[1].lastElementChild.value;
+    let provinciaDestinoSelec = infoFormulario[2].lastElementChild.value;
     let trayecto = `${provinciaOrigenSelec} hasta ${provinciaDestinoSelec}`;
-    let tituloTarjetaCarry = document.getElementById("tituloTarjetaCarry");
     tituloTarjetaCarry.innerText = trayecto;
     //mensaje del usuario
-    let mensaje = document.getElementById("mensajeCarry");
-    mensaje.innerText = infoFormulario[3].lastElementChild.value
+    mensajeCarry.innerText = document.getElementById("mensajeAPasajero").value;
     //fumar
-    let fumar = document.getElementById("fumarCarry");
-    fumar.innerText = infoFormulario[6].lastElementChild.value
+    if (infoFormulario[6].lastElementChild.value === "true") {
+        fumarCarry.innerText = mensajeFumador;
+    } else { fumarCarry.innerText = mensajeNoFumador}
     //mascota
-    let mascota = document.getElementById("mascotaCarry");
-    mascota.innerText = infoFormulario[7].lastElementChild.value
+    if (infoFormulario[7].lastElementChild.value === "true") {
+        mascotaCarry.innerText = mensajeAnimales;
+    } else {mascotaCarry.innerText = mensajeNoAnimales;};
     //maleta
-    let maleta = document.getElementById("maletaCarry");
-    maleta.innerText = infoFormulario[8].lastElementChild.value
+    if (infoFormulario[8][1][1].checked === "true") {
+        maletasCarry.innerText = mensajeMaleta;
+    } else { maletasCarry.innerText = mensajeNoMaleta}
     //peajes
-    let peajes = document.getElementById("peajeCarry");
-    peajes.innerText = infoFormulario[9].lastElementChild.value
+    if (infoFormulario[9][1][1].checked === "true") {
+        peajesCarry.innerText = mensajePeaje;
+    } else { peajesCarry.innerText = mensajeNoPeaje; }
     //precio
-    let precio = document.getElementById("precioCarry");
     precioTotalCarry();
-    preciototal = precioTotal
-    precio.innerText = precioTotal
+    precioCarry.innerText = precioTotal;
 });
 
-function precioTotalCarry(){
-    precioTotal = infoFormulario[10].lastElementChild.value * comision  }
