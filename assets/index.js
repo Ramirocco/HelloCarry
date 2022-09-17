f8946b4f9bda6eb87346641d2449f6bce1fd7528
Ramirocco <105560507+Ramirocco@users.noreply.github.com>
2022-09-17 16:26:01 -0300

    solucione problemas

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 //Variables y constantes
-const comision = 0.15;
+const comision = 1.15;
 //form
-let formularioCarry = document.getElementById("formularioCarry"); 
+let formularioCarry = document.getElementById("formularioCarry");
 //variables de partes de tarjeta
 let tituloTarjetaCarry = document.getElementById("tituloTarjetaCarry");
 let mensajeCarry = document.getElementById("mensajeCarry");
@@ -10,57 +10,79 @@ let fumarCarry = document.getElementById("fumarCarry");
 let maletaCarry = document.getElementById("maletaCarry");
 let peajesCarry = document.getElementById("peajesCarry");
 let precioCarry = document.getElementById("precioCarry");
-//mensajes para mostrar
-let mensajeFumador= "usted podria fumar en el vehiculo";
-let mensajeNoFumador = "usted NO podria fumar en el vehiculo";
-let mensajeAnimales= "usted podria llevar mascotas";
-let mensajeNoAnimales = "usted NO podria llevar mascotas";
-let mensajeMaleta= "usted podria llevar maleta";
-let mensajeNoMaleta = "usted NO podria llevar maletas";
-let mensajePeaje= "Los precios incluyen los peajes y gastos";
-let mensajeNoPeaje = "Los precios NO incluyen los peajes y gastos";
-
 //HTMLColection
 let infoFormulario;
-
 let precioTotal;
 let incluyePeajes;
-
+//funciones de precio a mostrar
 function precioTotalCarry() {
-    precioTotal = Number(infoFormulario[10].lastElementChild.value) * comision
+    precioTotal = Number(document.getElementById("precio").value) * comision
+};
+//mensajes para mostrar
+let mensajesTarjetas= {
+    fumador: "usted podria fumar en el vehiculo",
+    noFumador: "usted podria llevar mascotas",
+    noAnimales: "usted NO podria llevar mascotas",
+    animales: "usted podria llevar animales",
+    maleta: "usted podria llevar maleta",
+    noMaleta: "usted NO podria llevar maletas",
+    peaje: "Los precios incluyen los peajes y gastos",
+    noPeaje: "Los precios NO incluyen los peajes y gastos",
+    precioTotal: `El precio por pasajero es de $ ${precioTotal}`,
 };
 
 //codigo ejecutable
 formularioCarry.addEventListener("submit", (e) => {
     e.preventDefault();
     infoFormulario = e.target.children;
-    //ver info//
-    console.log(infoFormulario);
     //trayecto en tarjeta
-    let provinciaOrigenSelec = infoFormulario[1].lastElementChild.value;
-    let provinciaDestinoSelec = infoFormulario[2].lastElementChild.value;
+    let provinciaOrigenSelec = document.getElementById("provinciaOrigen").value;
+    let provinciaDestinoSelec = document.getElementById("provinciaDestino").value;
     let trayecto = `${provinciaOrigenSelec} hasta ${provinciaDestinoSelec}`;
     tituloTarjetaCarry.innerText = trayecto;
     //mensaje del usuario
     mensajeCarry.innerText = document.getElementById("mensajeAPasajero").value;
     //fumar
-    if (infoFormulario[6].lastElementChild.value === "true") {
-        fumarCarry.innerText = mensajeFumador;
-    } else { fumarCarry.innerText = mensajeNoFumador}
+    if (document.getElementById("fumar").checked === true) {
+        fumarCarry.innerText = mensajesTarjetas.fumador;
+    } else { fumarCarry.innerText = mensajesTarjetas.noFumador }
     //mascota
-    if (infoFormulario[7].lastElementChild.value === "true") {
-        mascotaCarry.innerText = mensajeAnimales;
-    } else {mascotaCarry.innerText = mensajeNoAnimales;};
+    if (document.getElementById("mascota").checked === true) {
+        mascotasCarry.innerText = mensajesTarjetas.animales;
+    } else { mascotaCarry.innerText = mensajesTarjetas.noAnimales;
+    };
     //maleta
-    if (infoFormulario[8][1][1].checked === "true") {
-        maletasCarry.innerText = mensajeMaleta;
-    } else { maletasCarry.innerText = mensajeNoMaleta}
+    if (document.getElementById("maleta").checked === true) {
+        maletasCarry.innerText = mensajesTarjetas.maleta;
+    } else { maletasCarry.innerText = mensajesTarjetas.noMaleta }
     //peajes
-    if (infoFormulario[9][1][1].checked === "true") {
-        peajesCarry.innerText = mensajePeaje;
-    } else { peajesCarry.innerText = mensajeNoPeaje; }
+    if (document.getElementById("peajes").checked === true) {
+        peajesCarry.innerText = mensajesTarjetas.peaje;
+    } else { peajesCarry.innerText = mensajesTarjetas.noPeaje; 
+    };
     //precio
     precioTotalCarry();
-    precioCarry.innerText = precioTotal;
+    precioCarry.innerText = mensajesTarjetas.precioTotal;
 });
 
+//Objeto del viaje 
+
+let nuevoViaje = {
+    //id = Math.random()*1000
+    origen: document.getElementById("provinciaOrigen").value,
+    destino: document.getElementById("provinciaDestino").value,
+    fumar: document.getElementById("fumar").checked,
+    mascotas: document.getElementById("mascota").checked,
+    maletas: document.getElementById("maleta").checked,
+    peajes: document.getElementById("peajes").checked,
+    precio: Number(document.getElementById("precio").value),
+}
+console.log (nuevoViaje);
+
+//prueba LOCALSTORAGE
+const dniInicio = document.getElementById(DniRegistro)
+sessionStorage.setItem
+
+
+let newArray3 = productos.map((productos) => productos.producto, productos.productos.precio * 8);
+console.log(newArray3);
